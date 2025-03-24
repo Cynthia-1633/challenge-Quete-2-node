78ee39d29902f1c461a26759971781c6180c76b3
Morelle <cynthia.morelle.cm@gmail.com>
2025-03-24 15:33:23 +0100

    create js application with an .env.sample

---
+++ challenge.js
@@ -0,0 +1,5 @@
+console.log(process.env.MY_NAME);
+console.log(process.env.MY_CITY);
+console.log(process.env.MY_LANGUAGE);
+
+console.log("I am " + process.env.MY_NAME + ",wilder in " + process.env.MY_CITY + " and I love " + process.env.MY_LANGUAGE + " .");
